57b439947c17d1ce3fcaa03c76fd31800dd2b345
ccandy <ccunandy2001@gmail.com>
2019-03-26 10:54:07 +0800

    finish moving object

---
+++ Assets/Script/Manager/Loader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject GameManager;
+    public GameObject SoundManager;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Script/Objects/MovingObjects.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObjects : MonoBehaviour {
+
+    // Use this for initialization
+
+    public float            moveTime = 0.1f;
+    public LayerMask        blockingPlayer;
+
+    private BoxCollider2D   boxCollider;
+    private Rigidbody2D     rb2D;
+    private float           inverseMoveTime;
+
+    protected virtual void Start()
+    {
+        boxCollider = gameObject.GetComponent<BoxCollider2D>();
+
+    }
+
+}
